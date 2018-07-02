48d4e94f1a1ff71ed51f9509f3e815c686297e75
Andy Leong <andy3leong@gmail.com>
2018-07-02 22:13:01 +0800

    adding apollo and dummy files

---
+++ src/config.js
@@ -0,0 +1,3 @@
+export const apiBackendUrl = process.env.REACT_APP_API_BACKEND_URL ?
+  process.env.REACT_APP_API_BACKEND_URL :
+  'http://localhost:4200'
--- src/index.js
+++ src/index.js
@@ -1,8 +1,23 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { ApolloProvider } from 'react-apollo'
+import { store, history } from 'store'
+import Routes from './routes'
+import apolloClient from './apollo'
+
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+/* Render app components */
+
+ReactDOM.render(
+  <ApolloProvider client={apolloClient}>
+    <Provider store={store}>
+      <Routes history={history} />
+    </Provider>
+  </ApolloProvider>,
+  document.getElementById('root')
+)
+
 registerServiceWorker();
